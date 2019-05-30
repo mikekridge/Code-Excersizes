448f05b4e777fa479a0bd2eed227e10ef7633227
MIke Ridge <mkradm@divvypay.com>
2019-05-30 09:08:05 -0600

    clean up

---
+++ test/time_test.exs
@@ -0,0 +1,35 @@
+defmodule Time.Test do
+  use ExUnit.Case
+
+  test "10 seconds from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "seconds")
+  end
+
+  test "10 minutes from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "minutes")
+  end
+
+  test "10 hours from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "hours")
+  end
+
+  test "10 days from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "days")
+  end
+
+  test "10 months from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "months")
+  end
+
+  test "10 years from now" do
+    assert {:ok, _result} = Time.time_from_now(10, "years")
+  end
+
+  test "invalid number entry" do
+    assert "Please enter a valid number or time type" = Time.time_from_now("test", "test")
+  end
+
+  test "invalid time type entry" do
+    assert "Please enter a valid number or time type" = Time.time_from_now(10, "test")
+  end
+end
